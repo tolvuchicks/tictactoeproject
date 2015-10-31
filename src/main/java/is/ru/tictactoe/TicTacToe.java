99df7ac2791f43c6aa1fe27c67442b62bf5a8a77
Asta <astagis@gmail.com>
2015-10-31 19:51:47 +0000

    added constructor

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -2,6 +2,16 @@ package is.ru.tictactoe;
 
 public class TicTacToe 
 {
+	public static int SIZE = 3;
+	private char[][] board;
+	private char humanPlayer;
+
+	public TicTacToe()
+	{
+		board = new char[SIZE][SIZE];
+		humanPlayer = 'x';
+	}
+
     public static int main(String text) 
     {
 		return 1;
