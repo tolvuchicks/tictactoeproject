7e58508c14175011b9ff20d916a515d086e38304
annagudrun <annag14@ru.is>
2015-11-02 20:01:53 +0000

    Some function added to main class

--- src/main/java/is/ru/tictactoe/TicTacToeGame.java
+++ src/main/java/is/ru/tictactoe/TicTacToeGame.java
@@ -7,34 +7,39 @@ public class TicTacToeGame
 	public static void main(String[] args)
 	{
 		TicTacToe game = new TicTacToe();
+		Scanner in = new Scanner(System.in);
 
-		game.printBoard();
-
+		System.out.println("Velkomin/nn í TicTacToe! Til þess að velja reit þarf að velja viðkomandi tölur:\n"
+			+ "Fyrri talan er x og seinni talan er y \n");
+		game.printBoardHelp();
+	
 		while(!game.checkFull() && !game.checkWin())
-		{
-			Scanner in = new Scanner(System.in);
-
-			System.out.println("Sladu inn x og y \n");
-
-			int x = in.nextInt();
-			int y = in.nextInt();
-
-			if(game.checkInput(x, y))
-			{
-				game.placeMark(x, y);
-				game.printBoard();
-				game.changePlayer();
-			}
+		{	 
+			 System.out.println("Sláðu inn x og y \n");
+		     int x = in.nextInt();
+		     int y = in.nextInt();
+		     
+		     if(game.checkInput(x, y))
+		     {
+		    	 game.placeMark(x, y);
+			     game.printBoard();
+			     game.changePlayer();
+		     }
+		     else
+		     {
+		    	 System.out.println("Vitlaust valið, gerðu aftur!");
+		     }
 		}
-
-		if(game.checkWin())
+		
+		if (game.checkWin()) 
 		{
-			System.out.println("Sigurvegari!");
+			System.out.println("Kæru gestir, við erum komin með sigurvegara!");
 			System.exit(0);
 		}
-		else if(game.checkFull())
+		else if (game.checkFull()) 
 		{
-			System.out.println("Enginn Sigurvegari");
+			System.out.println("Jafntefli!");
+			System.exit(0);
 		}
-	}
+		}
 }
