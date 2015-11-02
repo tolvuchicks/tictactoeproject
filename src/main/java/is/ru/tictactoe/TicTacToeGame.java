bb7bf8a86a20ff88884e4c1114f8428dd11babc6
Asta <astagis@gmail.com>
2015-11-02 23:20:36 +0000

    TicTacToeGame class refactored and output text edited

--- src/main/java/is/ru/tictactoe/TicTacToeGame.java
+++ src/main/java/is/ru/tictactoe/TicTacToeGame.java
@@ -9,37 +9,48 @@ public class TicTacToeGame
 		TicTacToe game = new TicTacToe();
 		Scanner in = new Scanner(System.in);
 
+		for(int i = 0; i < 45; i++)
+		{
+			System.out.println();
+		}
+		
 		System.out.println("Welcome to the TicTacToe Game! To select your field, please follow this format:\n"
 			+ "First x and then y (x y)\n");
 		game.printBoardHelp();
-	
+
 		while(!game.checkFull() && !game.checkWin())
-		{	 
-			 System.out.println("Please enter x and y\n");
-		     int x = in.nextInt();
-		     int y = in.nextInt();
+		{
+			System.out.println();
+			System.out.println("Please enter x and y\n");
+		    int x = in.nextInt();
+		    int y = in.nextInt();
 		     
-		     if(game.checkInput(x, y))
-		     {
-		    	 game.placeMark(x, y);
-			     game.printBoard();
-			     game.changePlayer();
-		     }
-		     else
-		     {
-		    	 System.out.println("That's not a valid selection, please try again!");
-		     }
+		    if(game.checkInput(x, y))
+		    {
+		    	game.placeMark(x, y);
+			    game.printBoard();
+			    game.changePlayer();
+		    }
+		    else
+		    {
+		     	System.out.println();
+		    	System.out.println("That's not a valid selection, please try again!");
+		    }
 		}
 		
 		if (game.checkWin()) 
 		{
-			System.out.println("~~ LADIES AND GENTLEMEN - WE HAVE A WINNER! ~~");
+			System.out.println("--------------------------------------------------");
+			System.out.println("| ~~ LADIES AND GENTLEMEN - WE HAVE A WINNER! ~~ |");
+			System.out.println("--------------------------------------------------");
 			System.exit(0);
 		}
 		else if (game.checkFull()) 
 		{
-			System.out.println("That's a tie!");
+			System.out.println("-----------------------");
+			System.out.println("| ~~ THAT'S A TIE! ~~ |");
+			System.out.println("-----------------------");
 			System.exit(0);
 		}
-		}
+	}
 }
