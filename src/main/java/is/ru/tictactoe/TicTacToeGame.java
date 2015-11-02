8c378a37428c7927e497992fd24a17a694173f73
Fjola8 <fjola14@ru.is>
2015-11-02 19:20:51 +0000

    TicTacToeGam main + build.gradle ready

--- src/main/java/is/ru/tictactoe/TicTacToeGame.java
+++ src/main/java/is/ru/tictactoe/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package is.ru.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame
 {
 	public static void main(String[] args)
@@ -7,5 +9,32 @@ public class TicTacToeGame
 		TicTacToe game = new TicTacToe();
 
 		game.printBoard();
+
+		while(!game.checkFull() && !game.checkWin())
+		{
+			Scanner in = new Scanner(System.in);
+
+			System.out.println("Sladu inn x og y \n");
+
+			int x = in.nextInt();
+			int y = in.nextInt();
+
+			if(game.checkInput(x, y))
+			{
+				game.placeMark(x, y);
+				game.printBoard();
+				game.changePlayer();
+			}
+		}
+
+		if(game.checkWin())
+		{
+			System.out.println("Sigurvegari!");
+			System.exit(0);
+		}
+		else if(game.checkFull())
+		{
+			System.out.println("Enginn Sigurvegari");
+		}
 	}
 }
